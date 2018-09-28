a5aaa3f3c8b8836fefba05675bd8e7253075bb6c
root <root@bastion.21d9.example.opentlc.com>
2018-09-28 11:33:16 +0000

    changed path to htpasswd file on masters

--- scripts/add_users_alpha_beta.sh
+++ scripts/add_users_alpha_beta.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for user in amy andrew brian betty; do htpasswd -b /root/htpasswd.openshift $user r3dh4t1!; done
+for user in amy andrew brian betty; do htpasswd -b /etc/origin/master/htpasswd $user r3dh4t1!; done
